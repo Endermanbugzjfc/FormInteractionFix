cb4ae2241aadf272c48ca057ea9f934bf21f7d5a
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-10-13 01:40:07 +0800

    Debug log.

--- src/PlayerSession.php
+++ src/PlayerSession.php
@@ -5,13 +5,46 @@ declare(strict_types=1);
 namespace Endermanbugzjfc\FormInteractionFix;
 
 use SOFe\AwaitGenerator\Await;
+use SOFe\AwaitGenerator\Channel;
+use SOFe\AwaitStd\AwaitStd;
 use pocketmine\event\EventPriority;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
+use pocketmine\player\Player;
 
 class PlayerSession {
 
-	public function eventLoop() : \Generator {
+	/**
+	 * @var Channel<null>
+	 */
+	private Channel $counter;
+
+	public function __construct(
+		private Player $player,
+		private AwaitStd $std,
+		\Logger $log
+	) {
+		Await::f2c(function () : \Generator {
+			try {
+				while ($this->player->isOnline()) {
+					yield from $this->mainLoop();
+				}
+			} catch (DisposeException $_) {
+				// Player quits.
+			}
+		});
+		Await::f2c(function () use ($log) : \Generator {
+			while ($this->player->isOnline()) {
+				$count = 0;
+				do {
+					$new = yield from $this->logLoop($count);
+				} while (is_int($new)); // Null = count unchanged in past 20 ticks.
+				$log->debug($new); // Cancelled X interactions from ...
+			})
+		});
+	}
+
+	public function mainLoop() : \Generator {
 		yield from $this->std->awaitEvent(
 			DataPacketSendEvent::class,
 			function (DataPacketSendEvent $event) : bool {
@@ -23,7 +56,7 @@ class PlayerSession {
 					}
 				}
 
-				return false;
+				return false; // Player is not one of the targets || packet is not form request.
 			},
 			false,
 			EventPriority::MONITOR,
@@ -43,7 +76,7 @@ class PlayerSession {
 				PlayerInteractEvent::class,
 				fn(PlayerInteractEvent $event) : bool => $event->getPlayer() === $this->player,
 				false,
-				EventPriority::LOW,
+				EventPriority::LOW, // One level ahead of NORMAL.
 				false,
 				$this->player
 			);
@@ -51,5 +84,15 @@ class PlayerSession {
 
 		if ($event instanceof PlayerInteractEvent) {
 			$event->cancel();
+			$this->counter->sendWithoutWait(null);
 		}
+	}
+
+	public function logLoop(int $count) : \Generator {
+		$new = yield from $this->std->timeout($this->counter->receive(), 20, $count);
+		if ($new !== null) {
+			return "Cancelled $new interactions from '{$this->player->getName()}' in the past 20 ticks";
+		}
+
+		return ++$count;
 	}
