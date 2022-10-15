e8d2be576217afb3e40e66f3257b56e1bc0fd762
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-10-16 00:34:41 +0800

    Block interaction.

--- src/PlayerSession.php
+++ src/PlayerSession.php
@@ -8,7 +8,10 @@ use SOFe\AwaitGenerator\Await;
 use SOFe\AwaitGenerator\Channel;
 use SOFe\AwaitStd\AwaitStd;
 use SOFe\AwaitStd\DisposeException;
+use pocketmine\event\EventPriority;
+use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\player\Player;
+use pocketmine\utils\Utils;
 
 class PlayerSession {
 
@@ -24,6 +27,8 @@ class PlayerSession {
 	 */
 	private Channel $responsed;
 
+	private bool $blockingInteraction = false;
+
 	public function __construct(
 		private Player $player,
 		private AwaitStd $std,
@@ -34,7 +39,22 @@ class PlayerSession {
 			$this->responsed
 		] = array_fill(0, 2, new Channel());
 
-		$this->loop($this->mainLoop());
+		$this->loop(function () {
+			$sent = yield from $this->sent->get();
+			$unblock = $this->blockInteraction();
+
+			$resp = yield from $this->resp->get();
+			if ($resp !== $sent) {
+				$this->weakError($this->player->getName() . "'s session expects $sent resp, got $resp resp");
+			}
+
+			$unblock();
+			return false; // This loop is supposed to only break when player quits and so returns false.
+		});
+	}
+
+	private function weakError(string $msg) : void {
+		$this->log->debug(implode("\n", Utils::printableExceptionInfo(new \RuntimeException($msg))));
 	}
 
 	/**
@@ -55,18 +75,35 @@ class PlayerSession {
 	}
 
 	/**
-	 * @return \Generator<mixed, mixed, mixed, bool> This loop is supposed to only break when player quits and so returns false.
+	 * @return callable Call to stop blocking interaction.
+	 * @throws \RuntimeException when called before unlocking the previous.
 	 */
-	public function mainLoop() : \Generator {
-		$sent = yield from $this->sent->get();
-		$unblock = $this->blockInteraction();
-
-		$resp = yield from $this->resp->get();
-		if ($resp !== $sent) {
-			$this->log->logException($this->player->getName() . "'s session expects $sent resp, got $resp resp");
+	public function blockInteraction() : callable {
+		if ($this->blockInteraction) {
+			throw new \RuntimeException("blockInteraction() called in " . $this->player->getName() . "'s session before unlock previous might be memory leak");
 		}
 
-		$unblock();
-		return false;
+		$this->blockInteraction = true;
+		$until = new Loading($controller = function () {
+			yield;
+			$this->blockingInteraction = false;
+		});
+
+		$this->loop(function () use ($until) {
+			yield from Await::race([
+				$this->std->awaitEvent(
+					PlayerInteractEvent::class,
+					fn($event) => $event->getPlayer() === $this->player,
+					false,
+					EventPriority::LOW, // One level ahead of NORMAL.
+					false,
+					$this->plasyer
+				),
+				$until->get()
+			]);
+		});
+
+		return fn() => $controller->rewind();
 	}
+
 }
