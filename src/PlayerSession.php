65df1456fb57e4ab86fb66f1c2179e96d7aa8f2d
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-11-06 23:31:42 +0800

    var_dump(): a b

--- src/PlayerSession.php
+++ src/PlayerSession.php
@@ -98,6 +98,7 @@ class PlayerSession {
 						default => true, // Filter out other that are useless to this plugin.
 					});
 
+			var_dump("b");
 					$closed = true;
 				} catch (DisposeException) {
 				}
@@ -115,6 +116,7 @@ class PlayerSession {
 					$this->player
 				);
 			} while (!$closed);
+			var_dump("a");
 		}
 	}
 }
