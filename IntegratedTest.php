0e2c8fb67325a96a39697b449b5af2f3cb3d29c3
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-11-06 18:51:47 +0800

    Forgot to return after kick.

--- IntegratedTest.php
+++ IntegratedTest.php
@@ -59,6 +59,7 @@ class IntegratedTest extends PluginBase implements Listener {
 	public function interactEveryTickWhenPlayerJoin(PlayerJoinEvent $event) : void {
 		if ($event->getPlayer()->getName() === "BlahCoast30765") {
 			$this->getScheduler()->scheduleDelayedTask(new ClosureTask(function () use ($event) : void { $event->getPlayer()->kick(); }), 1); // Cancelling PlayerLoginEvent crashes FakePlayer...
+			return;
 		}
 
 		if (isset($this->spammer)) {
